b062838f9c1430f9a68f696dc9c21217cb282a87
mazen <mazen.ezzeddine@gmail.com>
2022-02-28 18:21:32 +0100

    method refactoring WIP

--- src/main/java/org/hps/Controller.java
+++ src/main/java/org/hps/Controller.java
@@ -1,10 +1,17 @@
 package org.hps;
 
+import io.fabric8.kubernetes.api.model.ServiceAccount;
+import io.fabric8.kubernetes.api.model.ServiceAccountBuilder;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import org.apache.kafka.clients.admin.*;
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.KafkaFuture;
 import org.apache.kafka.common.TopicPartition;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -25,17 +32,12 @@ public class Controller {
     public static Map<TopicPartition, Long> currentPartitionToLastOffset = new HashMap<>();
     public static Map<TopicPartition, Long> partitionToLag = new HashMap<>();
 
-    public static String mode;
-    static boolean firstIteration = true;
+
     static Long sleep;
-    static Long waitingTime;
     static String topic;
     static String cluster;
     static Long poll;
-    static Long SEC;
-    static String choice;
     static String BOOTSTRAP_SERVERS;
-    static Long allowableLag;
     static Map<TopicPartition, OffsetAndMetadata> offsets;
 
 
@@ -47,25 +49,41 @@ public class Controller {
     ///////////////////////////////////////////////////////////////////////////
 
 
+    static Instant lastUpScaleDecision;
+    static Instant lastDownScaleDecision;
+    static boolean firstIteration= true;
+
+
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         readEnvAndCrateAdminClient();
+        lastUpScaleDecision = Instant.now();
+        lastDownScaleDecision = Instant.now();
+
+
+
 
         while (true) {
+            log.info("New Iteration:");
             getCommittedLatestOffsetsAndLag();
-            computeTotalArrivalRate();
+            //computeTotalArrivalRate();
+
+            log.info("Sleeping for {} seconds", sleep / 1000.0);
             Thread.sleep(sleep);
+
+            log.info("End Iteration;");
+            log.info("=============================================");
         }
 
 
     }
 
 
-    private static void readEnvAndCrateAdminClient(){
+    private static void readEnvAndCrateAdminClient() {
         sleep = Long.valueOf(System.getenv("SLEEP"));
         topic = System.getenv("TOPIC");
         cluster = System.getenv("CLUSTER");
-        //poll = Long.valueOf(System.getenv("POLL"));
+        poll = Long.valueOf(System.getenv("POLL"));
         CONSUMER_GROUP = System.getenv("CONSUMER_GROUP");
         BOOTSTRAP_SERVERS = System.getenv("BOOTSTRAP_SERVERS");
         Properties props = new Properties();
@@ -75,17 +93,20 @@ public class Controller {
     }
 
 
-
     private static void getCommittedLatestOffsetsAndLag() throws ExecutionException, InterruptedException {
         //get committed  offsets
-     offsets = admin.listConsumerGroupOffsets(CONSUMER_GROUP)
-                        .partitionsToOffsetAndMetadata().get();
+        offsets = admin.listConsumerGroupOffsets(CONSUMER_GROUP)
+                .partitionsToOffsetAndMetadata().get();
         numberOfPartitions = offsets.size();
+
+
+
         Map<TopicPartition, OffsetSpec> requestLatestOffsets = new HashMap<>();
         //initialize consumer to lag to 0
         for (TopicPartition tp : offsets.keySet()) {
             requestLatestOffsets.put(tp, OffsetSpec.latest());
             partitionToLag.put(tp, 0L);
+
         }
 
 
@@ -98,31 +119,52 @@ public class Controller {
             long latestOffset = latestOffsets.get(e.getKey()).offset();
             long lag = latestOffset - committedOffset;
 
-            previousPartitionToCommittedOffset.put(e.getKey(),
-                    currentPartitionToCommittedOffset.getOrDefault(e.getKey(),0L));
 
+            previousPartitionToCommittedOffset.put(e.getKey(),
+                    currentPartitionToCommittedOffset.get(e.getKey()));
             previousPartitionToLastOffset.put(e.getKey(),
-                    currentPartitionToLastOffset.getOrDefault(e.getKey(), 0L));
+                    currentPartitionToLastOffset.get(e.getKey()));
+
+
 
             currentPartitionToCommittedOffset.put(e.getKey(), committedOffset);
             currentPartitionToLastOffset.put(e.getKey(), latestOffset);
             partitionToLag.put(e.getKey(), lag);
+
+
+        }
+
+
+
+        ///////////////////////////////////////////////////
+
+        DescribeConsumerGroupsResult describeConsumerGroupsResult =
+                admin.describeConsumerGroups(Collections.singletonList(Controller.CONSUMER_GROUP));
+        KafkaFuture<Map<String, ConsumerGroupDescription>> futureOfDescribeConsumerGroupsResult =
+                describeConsumerGroupsResult.all();
+        consumerGroupDescriptionMap = futureOfDescribeConsumerGroupsResult.get();
+
+
+        if(!firstIteration){
+            computeTotalArrivalRate();
+        }else{
+            firstIteration = false;
         }
     }
 
 
-    private static void computeTotalArrivalRate(){
-        double totalConsumptionRate = 0;
-        double totalArrivalRate = 0;
+    private static void computeTotalArrivalRate() throws ExecutionException, InterruptedException {
+        double totalConsumptionRate;
+        double totalArrivalRate;
 
         long totalpreviouscommittedoffset = 0;
         long totalcurrentcommittedoffset = 0;
         long totalpreviousendoffset = 0;
         long totalcurrentendoffset = 0;
-        for(TopicPartition tp: offsets.keySet()) {
+        for (TopicPartition tp : offsets.keySet()) {
             totalpreviouscommittedoffset += previousPartitionToCommittedOffset.get(tp);
             totalcurrentcommittedoffset += currentPartitionToCommittedOffset.get(tp);
-            totalpreviousendoffset       +=   previousPartitionToLastOffset.get(tp);
+            totalpreviousendoffset += previousPartitionToLastOffset.get(tp);
             totalcurrentendoffset += currentPartitionToLastOffset.get(tp);
         }
 
@@ -133,12 +175,91 @@ public class Controller {
         log.info("totalArrivalRate {}, totalconsumptionRate {}",
                 totalArrivalRate * 1000, totalConsumptionRate * 1000);
 
+        log.info("time since last up scale decision is {}", Duration.between(lastUpScaleDecision, Instant.now()).toSeconds());
+        log.info("time since last down scale decision is {}", Duration.between(lastUpScaleDecision, Instant.now()).toSeconds());
+
+        youMightWanttoScale(totalArrivalRate);
 
     }
 
 
+    private static void youMightWanttoScale(double totalArrivalRate) throws ExecutionException, InterruptedException {
+        log.info("Inside you youMightWanttoScale");
+        int size = consumerGroupDescriptionMap.get(Controller.CONSUMER_GROUP).members().size();
+        log.info("curent group size is {}", size);
+
+        if (Duration.between(lastUpScaleDecision, Instant.now()).toSeconds() >= 30 ) {
+            log.info("Upscale logic, Up scale cool down has ended");
+
+            upScaleLogic(totalArrivalRate, size);
+        } else {
+            log.info("Not checking  upscale logic, Up scale cool down has not ended yet");
+        }
+
+
+        if (Duration.between(lastDownScaleDecision, Instant.now()).toSeconds() >= 60 ) {
+            log.info("DownScaling logic, Down scale cool down has ended");
+            downScaleLogic(totalArrivalRate, size);
+        }else {
+            log.info("Not checking  down scale logic, down scale cool down has not ended yet");
+        }
+    }
+
+    private static void upScaleLogic(double totalArrivalRate, int size) {
+        if ((totalArrivalRate * 1000) > size *poll) {
+            log.info("Consumers are less than nb partition we can scale");
+
+            try (final KubernetesClient k8s = new DefaultKubernetesClient()) {
+                ServiceAccount fabric8 = new ServiceAccountBuilder().withNewMetadata().withName("fabric8").endMetadata().build();
+                k8s.serviceAccounts().inNamespace("default").createOrReplace(fabric8);
+                k8s.apps().deployments().inNamespace("default").withName("cons1persec").scale(size + 1);
+
+                //  firstIteration = true;
+
+                log.info("Since  arrival rate {} is greater than  maximum consumption rate " +
+                        "{} ,  I up scaled  by one ", totalArrivalRate * 1000, size * poll);
+            }
+
+            lastUpScaleDecision = Instant.now();
+            lastDownScaleDecision = Instant.now();
+        }
+    }
+
+
+
+
+    private static void downScaleLogic(double totalArrivalRate, int size) {
+        if ((totalArrivalRate * 1000) < (size - 1) * poll) {
+
+            log.info("since  arrival rate {} is lower than maximum consumption rate " +
+                            " with size - 1  I down scaled  by one {}",
+                    totalArrivalRate * 1000, size * poll);
+            try (final KubernetesClient k8s = new DefaultKubernetesClient()) {
+                ServiceAccount fabric8 = new ServiceAccountBuilder().withNewMetadata().withName("fabric8").endMetadata().build();
+                k8s.serviceAccounts().inNamespace("default").createOrReplace(fabric8);
+                int replicas = k8s.apps().deployments().inNamespace("default").withName("cons1persec").get().getSpec().getReplicas();
+                if (replicas > 1) {
+                    k8s.apps().deployments().inNamespace("default").withName("cons1persec").scale(replicas - 1);
+                    lastDownScaleDecision = Instant.now();
+                    lastUpScaleDecision = Instant.now();
+
+                } else {
+                    log.info("Not going to  down scale since replicas already one");
+                }
+            }
+        }
+    }
+
+
+
 
 }
+
+
+
+
+
+
 
 
 
